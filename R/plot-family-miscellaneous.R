f707a3fd6042f0c253b681dff037a265e1cb58b7
Jan Kückelhaus <jankueckelhaus@gmx.de>
2020-09-13 20:38:10 +0200

    plot_segm change

--- R/plot-family-miscellaneous.R
+++ R/plot-family-miscellaneous.R
@@ -1439,7 +1439,7 @@ plotSegmentation <- function(object,
   ggplot2::ggplot() +
     ggplot2::geom_point(data = plot_df, size = pt_size, color = "lightgrey", mapping = ggplot2::aes(x = x, y = y)) +
     ggplot2::geom_point(data = segment_df, size = pt_size, mapping = ggplot2::aes(x = x, y = y, color = segment)) +
-    ggalt::geom_encircle(data = segment_df, alpha = 0.5, expand = 0.025, mapping = ggplot2::aes(x = x, y = y, group = segment, color = segment, fill = segment)) +
+    #ggalt::geom_encircle(data = segment_df, alpha = 0.5, expand = 0.025, mapping = ggplot2::aes(x = x, y = y, group = segment, color = segment, fill = segment)) +
     confuns::scale_color_add_on(aes = "fill", variable = "discrete", clrp = pt_clrp, guide = FALSE) +
     confuns::scale_color_add_on(aes = "color", variable = "discrete", clrp = pt_clrp) +
     ggplot2::theme_void() +
